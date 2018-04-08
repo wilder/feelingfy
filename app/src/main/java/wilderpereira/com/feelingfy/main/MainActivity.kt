10f4c3c0aaf235493a89f93245b73742f85c3a47
Wilder Pereira <wilder_roberto@hotmail.com>
2018-04-08 05:25:44 -0300

    add permissions

--- app/src/main/java/wilderpereira/com/feelingfy/main/MainActivity.kt
+++ app/src/main/java/wilderpereira/com/feelingfy/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package wilderpereira.com.feelingfy.main
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -19,6 +21,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ActivityCompat.requestPermissions(this@MainActivity,
+                arrayOf<String>(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                1);
+
 
         if (intent.extras!= null && intent.extras.containsKey("newPresentation")) {
             presentantionItem = intent.extras.get("newPresentation") as PresentationItem
